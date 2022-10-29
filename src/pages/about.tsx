292b1e2ce607ce54e452c031254c4c991789fba3
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2022-10-29 21:14:16 +0600

    updated code

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Head from "next/head";
+import * as ga from "../libs/GooglePageViews";
 
 type Props = {};
 
 const about = (props: Props) => {
+  const addToCarts = () => {
+    ga.event({
+      action: "add_carts",
+      category: "ecommerce",
+      label: "Item Added",
+      value: "Playing Card",
+    });
+  };
   return (
     <div>
       <Head>
         <title>about</title>
       </Head>
       <h1>About Pages</h1>
+      <a href="https://www.google.com">Go Google</a>
+      <button onClick={addToCarts}>Add price</button>
     </div>
   );
 };
