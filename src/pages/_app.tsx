555f0ceeac1fa47bb4aef21699ea4659087ff059
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2022-11-21 11:16:56 +0600

    added hotjar configurations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,16 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Script async id="hotjar">
+        {` (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3256488,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
       <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=G-23S03MS2ZP`}
